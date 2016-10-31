df21c4919cf2b86bb09590c12f426c43cc0dae51
Zou Jian <chsword@126.com>
2016-10-31 11:04:26 +0800

    add tests

---
+++ Fissoft.EntityFramework.Fts.Tests/Models/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fissoft.EntityFramework.Fts.Tests.Models
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext():base("DefaultConnection")
+        {
+
+        }
+        public DbSet<TestModel> TestModel { get; set; }
+    }
+
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Fissoft.EntityFramework.Fts.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Fissoft.EntityFramework.Fts.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fissoft.EntityFramework.Fts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            DbInterceptors.Init();
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var db = new MyDbContext())
+            {
+                db.Database.CreateIfNotExists();
+            }
+        }
+        [TestMethod]
+        public void MyTestMethod()
+        {
+           
+            using (var db = new MyDbContext())
+            {
+                db.Database.Log = (s =>
+                {
+                    Console.WriteLine(s);
+                });
+                var text = FullTextSearchModelUtil.Contains("a");
+                var query = db.TestModel
+                    .Where(c => c.Name.Contains(text))
+                    .ToList();
+                
+            }
+        }
+    }
+}
