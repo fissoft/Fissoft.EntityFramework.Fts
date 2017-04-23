0b6fb2d60364124d1abcc50cedaaf5de787c0014
Zou Jian <chsword@126.com>
2017-04-23 12:49:19 +0800

    F

---
+++ Fissoft.EntityFramework.Fts.Tests/Models/TestModel.cs
@@ -0,0 +1,9 @@
+namespace Fissoft.EntityFramework.Fts.Tests.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Name { get; set; }
+    }
+}
