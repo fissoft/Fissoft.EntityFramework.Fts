57d727c19dd86c5d7967c02d76915ec1c1e55cfe
Zou Jian <chsword@126.com>
2017-06-30 17:08:37 +0800

    Issue 7 add

---
+++ Fissoft.EntityFramework.Fts.Tests/Issue7Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Fissoft.EntityFramework.Fts.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fissoft.EntityFramework.Fts.Tests
+{
+    [TestClass]
+    public class Issue7Test
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            DbInterceptors.Init();
+        }
+        [TestMethod]
+        public void Test()
+        {
+            using (var db = new MyDbContext())
+            {
+                var first = db.TestModel.FirstOrDefault();
+
+                db.Database.Log = ConsoleUtil.Write;
+
+                var text = FullTextSearchModelUtil.Contains("web", true);
+                var query = db.TestModel
+                    .Where(c => c.Name.Contains(text))
+                    .ToList();
+              
+            }
+        }
+    }
+}
